d0b445dcdcd24a1fc83512fcb876c8928ed36e04
brett <brett.medina@utexas.edu>
2022-06-28 14:31:02 -0500

    tailwind setup

---
+++ src/components/InputField.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const InputField = () => {
+  return ( 
+    <form className="input">
+		<input
+			type="input"
+			placeholder='Enter a task'
+			className='input_box'></input>
+	</form>
+  )
+  
+}
+
+export default InputField
